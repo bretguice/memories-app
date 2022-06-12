0f4abc6755a2a7664d9fea86ffd6e2aa8f734007
bretguice <89871624+bretguice@users.noreply.github.com>
2022-06-12 17:48:02 -0500

    Added user access

--- src/App.js
+++ src/App.js
@@ -1,40 +1,29 @@
-import React, { useState, useEffect }from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
-import useStyles from './styles';
-import { useDispatch } from 'react-redux';
-import { getPosts } from './actions/posts';
-import memories from './images/memories.png'
+import React from 'react';
+import { Container } from '@material-ui/core';
+import {  GoogleOAuthProvider } from '@react-oauth/google';
+
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import Auth from './components/Auth/Auth';
+
 
 const App = () => {
-    const [currentId, setCurrentId] = useState(null);
-    const classes = useStyles();
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(getPosts());
-    }, [currentId, dispatch]);
-
+ 
     return(
+        <BrowserRouter>
         <Container maxidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit">
-                <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-                <img className={classes.image} src={memories} alt="memories" height="60" />
-            </AppBar>
-            <Grow in>
-                <Container>
-                    <Grid container className={classes.mainContainer} justifyContent="space-between" alignItems="stretch" spacing={3} >
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId} />
-                        </Grid>
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId} />
-                        </Grid>
-                    </Grid> 
-                </Container>
-            </Grow>
+            <Navbar />
+            <GoogleOAuthProvider clientId='636139791510-22pas2t4lphqs4ue4pd6icuade3pqt5r.apps.googleusercontent.com' >
+            <Routes>
+                <Route path='/' exact element={<Home />} />
+                <Route path='/auth' exact element={<Auth />} />    
+            </Routes>   
+            </GoogleOAuthProvider> 
         </Container>
+        </BrowserRouter>
     );
 }
 
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -6,10 +6,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import { createPost, updatePost } from '../../actions/posts';
 
 const Form = ({ currentId, setCurrentId }) => {
-    const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
-    const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null);
+    const [postData, setPostData] = useState({ title: '', message: '', tags: '', selectedFile: '' });
+    const post = useSelector((state) => currentId ? state.posts.find((message) => message._id === currentId) : null);
     const classes = useStyles();
     const dispatch = useDispatch();
+    const user = JSON.parse(localStorage.getItem('profile'));
 
     useEffect(() => {
         if(post) setPostData(post);
@@ -18,26 +19,35 @@ const Form = ({ currentId, setCurrentId }) => {
     const handleSubmit = (e) =>{
         e.preventDefault();
 
-        if(currentId){
-            dispatch(updatePost(currentId, postData))
+        if(currentId === 0){
+            dispatch(createPost({ ...postData, name: user?.result?.name }));
         } else {
-            dispatch(createPost(postData))
+            dispatch(updatePost(currentId, { ...postData, name: user?.result?.name }));
         }
         clear()
        
     }
 
     const clear = () =>{
-        setCurrentId(null);
-        setPostData({ creator: '', title: '', message: '', tags: '', selectedFile: '' });
+        setCurrentId(0);
+        setPostData({ title: '', message: '', tags: '', selectedFile: '' });
 
+    }
+
+    if(!user?.result?.name){
+        return(
+            <Paper className={classes.paper}>
+                <Typography variant='h6'align='center'>
+                    Please log in to create and like posts
+                </Typography>
+            </Paper>
+        )
     }
  
     return (
         <Paper className={classes.paper}>
             <form autoComplete='off' noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
                 <Typography variant="h6" >{currentId ? 'Editing' : 'Creating'} a Memory</Typography>
-                <TextField name='creator' variant='outlined' label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })}/>
                 <TextField name='title' variant='outlined' label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })}/>
                 <TextField name='message' variant='outlined' label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })}/>
                 <TextField name='tags' placeholder='seperate tags by commas' variant='outlined' label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.trim().split(',') })}/>
